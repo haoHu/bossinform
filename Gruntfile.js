8a95c86b5cc9f07b2916760deb3d310dee1a057a
haoHu <jackeysun1818@gmail.com>
2015-01-15 10:18:37 +0800

    update code for bi

--- Gruntfile.js
+++ Gruntfile.js
@@ -272,6 +272,13 @@ module.exports = function (grunt) {
 			}
 		},
 
+		// uglify
+		uglify : {
+			options : {
+				banner : "<%= config.banner %>"
+			}
+		},
+
 		// Reads HTML for usemin blocks to enable smart builds that automatically
 		// concat, minify and revision files, Creates configurations in memory so 
 		// additional tasks can operate on them
